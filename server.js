81be2cf35be6a2bf715576ce8b360cdf2e21c0df
AiSolace <sales@solaceai.dev>
2026-04-13 13:01:01 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
-const { handleChat } = require("./routes/chat");
+const { handleChat } = require("./routes/chat.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
